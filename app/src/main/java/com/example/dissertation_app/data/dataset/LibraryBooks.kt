e8d236df7966c106da4dd0faf3c81ffd158b210a
sk3ll <sk3ll69420@gmail.com>
2025-04-07 20:08:22 +0100

    Added a Library books data class/table which can store the books locally

--- app/src/main/java/com/example/dissertation_app/data/dataset/LibraryBooks.kt
+++ app/src/main/java/com/example/dissertation_app/data/dataset/LibraryBooks.kt
@@ -1,4 +1,14 @@
 package com.example.dissertation_app.data.dataset
 
-class LibraryBooks {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "library_books")
+data class LibraryBooks (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val author: String,
+    val description: String,
+    val imageUrl: String
+)
