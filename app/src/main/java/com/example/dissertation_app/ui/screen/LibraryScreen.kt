2ad40e5eeefbc68a47e7ba9fec202e63916bcf3d
sk3ll <sk3ll69420@gmail.com>
2025-04-11 19:14:49 +0100

    Added a changeable bottom screen which allows you to input text which can then be used to update the library name currently being used

--- app/src/main/java/com/example/dissertation_app/ui/screen/LibraryScreen.kt
+++ app/src/main/java/com/example/dissertation_app/ui/screen/LibraryScreen.kt
@@ -1,11 +1,16 @@
 package com.example.dissertation_app.ui.screen
 
+import android.R.attr.bottom
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,14 +19,22 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.CloudDownload
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -58,6 +71,9 @@ fun LibraryScreen(
     val bookViewModel: BookViewModel? = SearchLocation.getBookViewModel()
     val bookFragment = BookFragment(libraryBookViewModel!!)
 
+    var showCreateLibrary by remember { mutableStateOf(false) }
+    var currentLibraryName by remember { mutableStateOf("") }
+
     Scaffold (
         topBar = {
             BookTopAppBar(
@@ -68,17 +84,31 @@ fun LibraryScreen(
         },
 
         bottomBar = {
-            AddNewLibraries(
-                circularButtonFunction = {},
-                removeButtonFunction = {},
-                uploadingButtonFunction = {}
-            )
+
+            if (showCreateLibrary) {
+                CreateLibrary(
+                    createFunction = {
+                        currentLibraryName = it
+                    },
+                    exitFunction = {
+                        showCreateLibrary = false
+                    }
+                )
+            } else {
+                AddNewLibraries(
+                    circularButtonFunction = {
+                        showCreateLibrary = true
+                    },
+                    removeButtonFunction = {},
+                    uploadingButtonFunction = {}
+                )
+            }
         }
     ) {
         Column(
             modifier = Modifier.padding(it)
         ) {
-            Text(text = "Library Screen")
+            Text(text = currentLibraryName)
         }
     }
 }
@@ -93,11 +123,10 @@ fun AddNewLibraries(
 ) {
     Row(
         modifier = modifier
-        .padding(10.dp)
-        .background(Color.Gray)
-        .border(1.dp, Color.White)
-        .height(200.dp)
-        .width(500.dp),
+            .background(Color.Gray)
+            .border(1.dp, Color.White)
+            .height(200.dp)
+            .width(500.dp),
 
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
@@ -136,6 +165,86 @@ fun AddNewLibraries(
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateLibrary(
+    modifier: Modifier = Modifier,
+    createFunction: (String) -> Unit = {},
+    exitFunction: () -> Unit = {}
+) {
+    var textValue by remember { mutableStateOf("") }
+    Box(
+        modifier = modifier
+            .background(Color.Gray)
+            .border(1.dp, Color.White)
+            .height(200.dp)
+            .width(500.dp)
+    ) {
+        IconButton(
+            onClick = exitFunction,
+            modifier = Modifier.align(Alignment.TopEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = "close",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(30.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(text = "Create Library",
+                style = MaterialTheme.typography.titleMedium,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            TextField(
+                value = textValue,
+                onValueChange = {
+                    textValue = it
+                    Log.d("LibraryScreen", "Library name: $textValue")
+                },
+                placeholder = { Text("Enter library name") },
+                modifier = Modifier
+                    .background(Color.White)
+            )
+        }
+
+        IconButton(
+            onClick = {
+                createFunction(textValue)
+                exitFunction()
+            },
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "add",
+            tint = Color.White,
+            modifier = Modifier
+                .size(30.dp)
+        )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun CreateLibraryPreview() {
+    CreateLibrary()
 }
 
 @Preview
