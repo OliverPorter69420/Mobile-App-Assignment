68af1ba0d9ad514e013f6263e7470b67b47c72bc
OliverPorter69420 <sk3ll69420@gmail.com>
2025-04-15 23:04:31 +0100

    Added previews and 3 different screens for the options of the uiState

--- app/src/main/java/com/example/dissertation_app/ui/items/LibraryBookMenu.kt
+++ app/src/main/java/com/example/dissertation_app/ui/items/LibraryBookMenu.kt
@@ -1,6 +1,18 @@
 package com.example.dissertation_app.ui.items
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells.Fixed
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LibraryBookMenu(
@@ -9,7 +21,84 @@ fun LibraryBookMenu(
     getBooks: (Int) -> Unit = {},
     deleteBook: (Int,Int) -> Unit
 ) {
-    /*todo finish this part of the program and then improve the add library book functionality
-    *  to add books to the library the user chooses and then add some small fixes (this is all
-    *  for adding accounts in later on after this is all sorted*/
+
+    when (uiState) {
+        is SavedLibraryUiState.Success -> LibraryBookGrid(
+            modifier = Modifier
+        )
+
+        SavedLibraryUiState.Empty -> EmptyLibraryBookScreen(
+            modifier = Modifier
+        )
+
+        SavedLibraryUiState.Error -> ErrorScreenBookScreen(
+            modifier = Modifier
+        )
+    }
+        /* todo finish this part of the program and then improve the add library book functionality
+    * to add books to the library the user chooses and then add some small fixes (this is all
+    * for adding accounts in later on after this is all sorted*/
+
+}
+
+@Composable
+fun ErrorScreenBookScreen(
+    modifier : Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Error loading this Library, try again!")
+    }
+}
+
+@Composable
+fun EmptyLibraryBookScreen(
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("No book within this Library, maybe add some into the library")
+    }
+}
+
+@Composable
+fun LibraryBookGrid(
+    modifier: Modifier
+) {
+    LazyVerticalGrid(
+        columns = Fixed(1),
+        modifier = Modifier.padding(10.dp)
+    ) {
+        
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorScreenBookScreenPreview() {
+    ErrorScreenBookScreen(
+        modifier = Modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EmptyLibraryBookScreenPreview() {
+    EmptyLibraryBookScreen(
+        modifier = Modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LibraryBookGridPreview() {
+
 }
