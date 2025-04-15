6dbf248c1bb14222fbcd64673a1c8dc99b07fdbc
OliverPorter69420 <sk3ll69420@gmail.com>
2025-04-15 11:11:44 +0100

    Updated book thumbnail array.

--- app/src/main/java/com/example/dissertation_app/ui/items/BookMenu.kt
+++ app/src/main/java/com/example/dissertation_app/ui/items/BookMenu.kt
@@ -362,14 +362,14 @@ fun PhotoGridPreview() {
     )
 
     val thumbnails : Array<String> = arrayOf(
-        "test",
-        "test",
-        "test",
-        "test",
-        "test",
-        "test",
-        "test",
-        "test"
+        "test1",
+        "test2",
+        "test3",
+        "test4",
+        "test5",
+        "test6",
+        "test7",
+        "test8"
     )
 
     BookGridScreen(
